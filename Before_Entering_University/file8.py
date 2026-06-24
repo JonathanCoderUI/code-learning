ecc3dc1384fcf4639f5d0b43ec0a290b5f78239b
Jonathan Kana <jokana2008@gmail.com>
2026-06-24 10:35:21 +0000

    List Variable, For, If & Else

---
+++ Before_Entering_University/file8.py
@@ -0,0 +1,7 @@
+nilai_peserta = [45, 85, 60, 90]
+
+for nilai in nilai_peserta:
+    if nilai >= 70:
+        print("Peserta dengan nilai", nilai, "dinyatakan: LOLOS")
+    else:
+        print("Peserta dengan nilai", nilai, "dinyatakan: GAGAL")
